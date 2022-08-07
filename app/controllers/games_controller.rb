4ac83760a0d26fb8acc145c80d8115f7f0e8ec3a
Your Name <107790440+daikingkong@users.noreply.github.com>
2022-08-07 14:41:17 +0000

    [add] games_index_right(games_index)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,6 +8,30 @@ class GamesController < ApplicationController
     @user = current_user
   end
 
+  def create
+    game = Game.new(game_params)
+    game.user_id = current_user.id
+    if game.save
+      redirect_to game_path(game)
+    else
+      @games = Game.all
+      @user = current_user
+      render :index
+    end
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :body)
   end
 end
